d7782f815e8d8c06ca4dee8790af1cde0cd2d84a
Danny Becerra <ddnielbec@outlook.com>
2022-05-31 16:04:56 -0500

    Inicio de pagina

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  }
+})
+
+// ======MOSTRAR MENU=========
+
+const navMenu = document.getElementById('nav-menu')
+const navOpen = document.getElementById('menu-button')
+const navClose = document.getElementById('menu-close')
+
+if (navOpen){
+  navOpen.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+  })
+}
+
+if (navClose){
+  navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+/*REMOVER MENU MOVIL AL SELECCIONAR UNA PESTANA*/
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
